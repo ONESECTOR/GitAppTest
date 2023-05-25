2acc249f1376a96a05a38cf397df8ef3d9cb183a
sector <75115941+onesector@users.noreply.github.com>
2023-05-25 18:17:20 +0300

    release 2

--- app/src/main/java/com/sector/gitapptest/FirstFragment.kt
+++ app/src/main/java/com/sector/gitapptest/FirstFragment.kt
@@ -1,6 +1,7 @@
 package com.sector.gitapptest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,16 +13,27 @@ class FirstFragment : Fragment() {
     private var _binding: FragmentFirstBinding? = null
     private val binding get() = _binding!!
 
+    private var health = 1000
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+        foo()
+
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun foo() {
+        if (health == 1000) {
+            Log.d("TAG!", "Alive")
+        }
     }
 }
